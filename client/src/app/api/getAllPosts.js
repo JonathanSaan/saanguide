99d5fabbdb111909979953592752ac1b5bc2290f
jonathansaandev <jonathansaandev@gmail.com>
2023-07-28 20:23:25 -0300

    api

--- client/src/app/api/getAllPosts.js
+++ client/src/app/api/getAllPosts.js
@@ -3,9 +3,14 @@ import axios from "axios";
 export default async function getAllPosts() {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_BACK_URL}/posts`);
+
+    if (!response.data) {
+      return null;
+    }
+
     return response.data;
-    
   } catch (error) {
     console.log(`Error fetching all posts: ${error.message}`);
+    return null
   }
 }
--- client/src/app/api/getPost.js
+++ client/src/app/api/getPost.js
@@ -3,9 +3,14 @@ import axios from "axios";
 export default async function getPost(slug) {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_BACK_URL}/post/${slug}`);
-    return response.data;
 
+    if (!response.data) {
+      return null;
+    }
+    
+    return response.data;
   } catch (error) {
     console.log(`Error fetching the post: ${error.message}`);
+    return null;
   }
 }
