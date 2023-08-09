f560b8dcec527bdf435b15b4722c5a4bd9858679
jonathansaandev <jonathansaandev@gmail.com>
2023-08-09 17:32:09 -0300

    article details

--- client/src/app/[slug]/page.js
+++ client/src/app/[slug]/page.js
@@ -6,24 +6,27 @@ import moment from "moment";
 import getAllPosts from "../api/getAllPosts";
 import getPost from "../api/getPost";
 import Header from "../components/header";
+import Comments from "../components/comment";
 import Footer from "../components/footer";
 import styles from "../styles/post.module.scss";
 
 export async function generateMetadata({ params }) {
-  const post = await getPost(params.slug);
-  if (!post)
+  try {
+    const post = await getPost(params.slug);
+
+    return {
+      title: `${post.title} - Saan's Guidebook`,
+      description: `An article by ${post.title} - Saan's Guidebook`,
+      alternates: {
+        canonical: `/${post.slug}`,
+      },
+    };
+  } catch (error) {
     return {
       title: "Not Found - Saan's Guidebook",
       description: "The page you are looking for does not exist.",
     };
-
-  return {
-    title: `${post.title} - Saan's Guidebook`,
-    description: post.description,
-    alternates: {
-      canonical: `/${post.slug}`,
-    },
-  };
+  }
 }
 
 export async function generateStaticParams() {
@@ -39,7 +42,7 @@ export async function generateStaticParams() {
 }
 
 const Post = async ({ params }) => {
-  const post = await getPost(params.slug) ?? null;
+  const post = await getPost(params.slug);
 
   if (!post) notFound();
 
@@ -47,35 +50,24 @@ const Post = async ({ params }) => {
     <div className={styles.post}>
       <Header />
       <main className={styles.post_container}>
-        {post ? (
-          <article key={post.id}>
-            <header>
-              <h1 className={styles.post_containerTitle}>{post.title}</h1>
-              <Image
-                src={post.banner}
-                className={styles.post_containerImage}
-                width={800}
-                height={500}
-                alt="Picture of the post"
-              />
-            </header>
-            <span className={styles.post_container_details}>
-              <p className={styles.post_container_detailsAuthor}>{post.author}</p>
-              <p className={styles.post_container_detailsDate}>{moment(post.createdAt).format("MMM DD, YYYY")}</p>
-            </span>
-            <p className={styles.post_containerWarn}>{post.warn}</p>
-            {post.text.map((one) => (
-              <div key={one.id}>
-                <h2 className={styles.post_containerSubtitle}>
-                  {one.subtitle}
-                </h2>
-                <p className={styles.post_containerParagraph}>
-                  {one.paragraph}
-                </p>
-              </div>
-            ))}
-          </article>
-        ) : null}
+        <article key={post.id}>
+          <header>
+            <h1 className={styles.post_containerTitle}>{post.title}</h1>
+            <Image
+              src={post.banner}
+              className={styles.post_containerImage}
+              width={800}
+              height={500}
+              alt="Picture of the post"
+            />
+          </header>
+          <span className={styles.post_container_details}>
+            <p className={styles.post_container_detailsAuthor}>{post.author}</p>
+            <p className={styles.post_container_detailsDate}>{moment(post.createdAt).format("MMM DD, YYYY")}</p>
+          </span>
+          <div className={styles.post_container_description} dangerouslySetInnerHTML={{ __html: post.description }}></div>
+          <Comments slug={params.slug} />
+        </article>
       </main>
       <Footer />
     </div>
