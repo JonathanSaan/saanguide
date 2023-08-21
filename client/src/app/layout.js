ba4632b16b6aca53940b621daadf8d536879a44b
jonathansaandev <jonathansaandev@gmail.com>
2023-08-21 20:25:42 -0300

    font

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,4 +1,4 @@
-import { Roboto } from "next/font/google";
+import { Roboto_Condensed } from "next/font/google";
 
 import { UserProvider } from "./UserContext.js";
 import CheckCookie from "./checkCookie";
@@ -13,15 +13,16 @@ export const metadata = {
   description: "A blog about interesting things of which I have a certain domain.",
 };
 
-const roboto = Roboto({
+const roboto_condensed = Roboto_Condensed({
   weight: ["300", "400"],
   subsets: ["latin"],
+  display: "swap",
 })
 
 export default function RootLayout({ children }) {
   return (
     <UserProvider>
-      <html lang="en" className={roboto.className}>
+      <html lang="en">
         <body suppressHydrationWarning={true}>
           <CheckCookie>{children}</CheckCookie>
         </body>
