539378d3b3a233d8f941d9f2d89c418d41827d2b
jonathansaandev <jonathansaandev@gmail.com>
2024-04-07 20:14:30 -0300

    Refactor UserContext.js and remove isAdmin check in useEffect

--- client/src/app/UserContext.js
+++ client/src/app/UserContext.js
@@ -7,20 +7,15 @@ const UserContext = createContext();
 
 const UserProvider = React.memo(({ children }) => {
   const userString = Cookies.get("user");
-  
   const [showConsent, setShowConsent] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState();
   const [isAdmin, setIsAdmin] = useState();
   
   useEffect(() => {
     const user = typeof userString !== "undefined" ? JSON.parse(userString) : null;
-    setIsLoggedIn(user);
-
-    if (user && user.isAdmin !== undefined) {
-      setIsAdmin(user.isAdmin);
-    }
-
     const cookieConsent = Cookies.get("showConsent");
+    
+    setIsLoggedIn(user);
     setShowConsent(cookieConsent !== "false");
   }, [userString]);
 
