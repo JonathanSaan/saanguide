2b928623bed7acf227f26b3331a503244310aab8
JonathanSaan <jonathansaandev@gmail.com>
2024-06-20 17:01:19 -0300

    chore: Update paths in jsconfig.json and next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  compilerOptions: {
-    baseUrl: './',
-    paths: {
-      '@vercel/analytics/react': ['./node_modules/@vercel/analytics/dist/react'],
-    },
-  },
   experimental: {
     serverActions: true,
   },
   images: {
-    domains: ['localhost', 'encrypted-tbn1.gstatic.com', 'c.pxhere.com'],
+    domains: ["localhost", "encrypted-tbn1.gstatic.com", "c.pxhere.com"],
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
